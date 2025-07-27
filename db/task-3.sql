e5d4bca2a1cb5c4a551f2c9bc220e33e5f47690b
Introvert2000 <alvareschamp200@gmail.com>
2025-07-27 17:58:34 +0530

    Add initial user data and basic queries for college_db
    - Insert multiple user records into the users table.
    - Update the department of a user with id 1.
    - Delete the user with id 1.
    - Add various SELECT queries to retrieve user data based on different conditions.

---
+++ db/task-3.sql
@@ -0,0 +1,8 @@
+INSERT INTO college_db.users (name, age, department, email) VALUES ( 'gadekar', '23', 'comp', 'gadekar@gmail.com')
+INSERT INTO college_db.users (name, age, department, email) VALUES ( 'ved', '23', 'comp', 'ved@gmail.com')
+INSERT INTO college_db.users (name, age, department, email) VALUES ( 'aaron', '23', 'comp', 'aaron@gmail.com')
+INSERT INTO college_db.users (name, age, department, email) VALUES ( 'avnish', '23', 'comp', 'avnish@gmail.com')
+INSERT INTO college_db.users (name, age, department, email) VALUES ( 'vaibhav', '23', 'comp', 'vaibhav20@gmail.com')
+
+UPDATE college_db.users SET department = 'etc' WHERE id = 1
+DELETE FROM college_db.users WHERE user.id = 1
---
+++ db/task-4.sql
@@ -0,0 +1,4 @@
+SELECT * FROM `college_db`.`users` 
+SELECT * FROM `college_db`.`users` WHERE `department` = 'comp'
+SELECT * FROM `college_db`.`users` WHERE `age` > 20
+SELECT * FROM `college_db`.`users` WHERE `department` != 'comp'
